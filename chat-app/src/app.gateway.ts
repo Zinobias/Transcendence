a05155d8fa4aac81e858b6c1bdecc5555f6907e6
Teriuihi <teriuihi@alttd.com>
2022-10-26 17:54:11 +0200

    Removed db test query

--- chat-app/src/app.gateway.ts
+++ chat-app/src/app.gateway.ts
@@ -53,11 +53,6 @@ export class AppGateway
 
   afterInit(server: Server) {
     this.logger.log('Init');
-    Queries.getInstance()
-      .addUser('abc', 'abi is learning')
-      .then((r) => {
-        this.logger.log('Abba was added to the database ig?');
-      });
   }
 
   handleDisconnect(client: Socket) {
