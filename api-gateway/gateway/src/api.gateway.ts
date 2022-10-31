35de63b8dee59e6f3f64ce3ca8a6b9e8d1276567
zinobias <zinobias@live.nl>
2022-10-31 18:55:27 +0100

    added namespace option to websocketgateway options

--- api-gateway/gateway/src/api.gateway.ts
+++ api-gateway/gateway/src/api.gateway.ts
@@ -7,6 +7,7 @@ import { Socket, Server } from 'socket.io';
 @WebSocketGateway(8081 ,{
 	path: '/', // can look into path
 	serveClient: true,
+	namespace: '/',
 })
 export class ApiGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect	{
 	private clientList : {userID : number,
