73cdb86efd273e66cdaadb6ca50ff0db675cfcde
zinobias <zinobias@live.nl>
2022-10-31 19:16:25 +0100

    added basic methods to gateways

--- api-gateway/gateway/src/chat.gateway.ts
+++ api-gateway/gateway/src/chat.gateway.ts
@@ -1,9 +1,32 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { Server } from 'typeorm';
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
+@WebSocketGateway(8081, {
+	path: '/', // can look into path
+	serveClient: true,
+	namespace: '/chat',
+  })
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+	private logger: Logger = new Logger('chat gateway');
+ 
+	@SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+    return 'Hello world!';}
+
+	afterInit(server: Server) {
+		this.logger.log('Initialized');
+		}
+
+	handleConnection(client: Socket, ...args: any[]) {
+	this.logger.log('Client connected: ${client.id}');
+	}
+
+	handleDisconnect(client: Socket) {
+	this.logger.log('Client disconnected: ${client.id}');
+	}
+
+	@WebSocketServer() wss: Server;
+
 }
