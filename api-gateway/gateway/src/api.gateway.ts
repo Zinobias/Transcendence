a00057b569d8c58bfff6da35e12de61c0377f338
Teriuihi <teriuihi@alttd.com>
2022-10-31 18:57:14 +0100

    Rewrote auth to be easier to read

--- api-gateway/gateway/src/api.gateway.ts
+++ api-gateway/gateway/src/api.gateway.ts
@@ -1,41 +1,49 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Queries } from './database/queries';
 import { randomUUID } from 'crypto';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway(8081 ,{
-	path: '/', // can look into path
-	serveClient: true,
-	namespace: '/',
+@WebSocketGateway(8081, {
+  path: '/', // can look into path
+  serveClient: true,
+  namespace: '/',
 })
-export class ApiGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect	{
-	private clientList : {userID : number,
-	};
+export class ApiGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private clientList: { userID: number };
 
-	constructor(@Inject(Auth) private readonly auth: Auth) {}
+  constructor(@Inject(Auth) private readonly auth: Auth) {}
 
-	afterInit(server: Server) {
-		this.logger.log("Initialized");
-	}
+  afterInit(server: Server) {
+    this.logger.log('Initialized');
+  }
 
-	handleConnection(client: Socket, ...args: any[]) {
-		this.logger.log('Client connected: ${client.id}');
-	}
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log('Client connected: ${client.id}');
+  }
 
-	handleDisconnect(client: Socket) {
-		this.logger.log('Client disconnected: ${client.id}');
-	}
+  handleDisconnect(client: Socket) {
+    this.logger.log('Client disconnected: ${client.id}');
+  }
 
-	@WebSocketServer() wss : Server;
-	private logger : Logger = new Logger("AppGateway");
+  @WebSocketServer() wss: Server;
+  private logger: Logger = new Logger('AppGateway');
 
-	@SubscribeMessage('message')
-	handleMessage(client: any, payload: any): string {
-	//client = chat-appendFile
-	//client.send(pattern, payload);
-	return 'Hello world!';
-	}
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    //client = chat-appendFile
+    //client.send(pattern, payload);
+    return 'Hello world!';
+  }
 }
 
 @Injectable()
@@ -76,50 +84,61 @@ export class Auth {
   }
 
   @SubscribeMessage('auth')
-  auth(client: Socket, data: AuthData) {
-    fetch('https://api.intra.42.fr/v2/oauth/token', {
-      method: 'Post',
-      body: JSON.stringify({
-        grant_type: 'authorization_code',
-        client_id: process.env.CLIENT,
-        client_secret: process.env.SECRET,
-        code: data.code,
-        redirect_uri: 'http://localhost:3000',
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((response) => {
-      response.json().then((result: AuthToken) => {
-        console.log(result.access_token);
-        fetch('https://api.intra.42.fr/v2/me', {
-          method: 'Get',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + result.access_token,
-          },
-        }).then((secondResponse) => {
-          secondResponse.json().then((secondResult) => {
-            console.log(secondResult.id);
-            this.sockets.storeSocket(secondResult.id, client);
-            const uuid = randomUUID();
-            Queries.getInstance()
-              .storeAuth(secondResult.id, uuid)
-              .then(async (success) => {
-                if (success) {
-                  await this.updateAuth(secondResult.id);
-                  client.emit('user_id', {
-                    user_id: secondResult.id,
-                    auth_cookie: uuid,
-                  });
-                } else {
-                  client.emit('auth_failed', {
-                    msg: 'Unable to store auth token',
-                  });
-                }
-              });
-          });
-        });
-      });
+  async auth(client: Socket, data: AuthData) {
+    const oauthResponse = await fetch(
+      'https://api.intra.42.fr/v2/oauth/token',
+      {
+        method: 'Post',
+        body: JSON.stringify({
+          grant_type: 'authorization_code',
+          client_id: process.env.CLIENT,
+          client_secret: process.env.SECRET,
+          code: data.code,
+          redirect_uri: 'http://localhost:3000',
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+    const json: AuthToken = await oauthResponse.json();
+    const userId = await this.retrieveUserId(client, json);
+    const uuid = randomUUID();
+    this.storeSession(client, userId, uuid);
+  }
+
+  private async retrieveUserId(
+    client: Socket,
+    authToken: AuthToken,
+  ): Promise<number> {
+    console.log(authToken.access_token);
+    const response = await fetch('https://api.intra.42.fr/v2/me', {
+      method: 'Get',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + authToken.access_token,
+      },
     });
+    const json = await response.json();
+    console.log(json.id);
+    this.sockets.storeSocket(json.id, client);
+    return json.id;
+  }
+
+  private storeSession(client: Socket, userId: number, uuid: string) {
+    Queries.getInstance()
+      .storeAuth(userId, uuid)
+      .then(async (success) => {
+        if (success) {
+          await this.updateAuth(userId);
+          client.emit('user_id', {
+            user_id: userId,
+            auth_cookie: uuid,
+          });
+        } else {
+          client.emit('auth_failed', {
+            msg: 'Unable to store auth token',
+          });
+        }
+      });
   }
 }
 
