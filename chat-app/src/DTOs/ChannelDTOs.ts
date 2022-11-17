1bf55745d483b2ac303ca670482b26051238be63
Stijn <sappunn@student.codam.nl>
2022-11-17 22:38:27 +0100

    Improved logging and started work on better responses to front end if something fails

---
+++ chat-app/src/DTOs/ChannelDTOs.ts
@@ -0,0 +1,55 @@
+export interface ChannelError {
+    reason: string
+}
+
+export interface ChannelCreate {
+    creator_id: number;
+    channel_name: string;
+    creator2_id: number;
+}
+
+export interface ChannelJoin {
+    channel_id: number;
+    user_id: number;
+}
+
+export interface ChannelLeave {
+    channel_id: number;
+    user_id: number;
+}
+
+export interface ChannelPromote {
+    channel_id: number;
+    user_id: number;
+    actor_id: number;
+}
+
+export interface ChannelDemote {
+    channel_id: number;
+    user_id: number;
+    actor_id: number;
+}
+
+export interface ChannelKick {
+    channel_id: number;
+    user_id: number;
+    actor_id: number;
+}
+
+export interface ChannelBan {
+    channel_id: number;
+    user_id: number;
+    actor_id: number;
+    until: number;
+}
+
+export interface ChannelDisband {
+    channel_id: number;
+    user_id: number;
+}
+
+export interface ChannelMessage {
+    channel_id: number;
+    user_id: number;
+    message: string;
+}
--- chat-app/src/Listeners/Util.ts
+++ chat-app/src/Listeners/Util.ts
@@ -1,118 +1,99 @@
-import { Channel } from '../Objects/Channel';
-import { Inject, Injectable, Logger } from '@nestjs/common';
-import { User } from '../Objects/User';
-import { microServiceDTO } from '../app.controller';
-import { AppService } from '../app.service';
-import { ClientProxy } from '@nestjs/microservices';
+import {Channel} from '../Objects/Channel';
+import {Inject, Injectable, Logger} from '@nestjs/common';
+import {User} from '../Objects/User';
+import {microServiceDTO} from '../app.controller';
+import {AppService} from '../app.service';
+import {ClientProxy} from '@nestjs/microservices';
 
 @Injectable()
 export class Util {
-  constructor(@Inject('gateway') private readonly gateway: ClientProxy) {}
-  //EZ MESSAGE:
-
-  public notify(userIds: number[], channel: string, args: object) {
-    const obj: microServiceDTO = {
-      eventPattern: channel,
-      userIDs: userIds,
-      data: args,
-    };
-    this.gateway.emit('chat', obj);
-  }
-
-  //EZ ERRORS:
-
-  public getChannel(channelId: number, source: string): Channel {
-    const channel: Channel = Channel.getChannel(channelId);
-    if (channel == null) {
-      Logger.warn(
-        'Received invalid channel id [' + channelId + '] from ' + source,
-      );
-      return undefined;
+    constructor(@Inject('gateway') private readonly gateway: ClientProxy) {
     }
-    return channel;
-  }
 
-  public async getUser(
-    userId: number,
-    source: string,
-  ): Promise<User | undefined> {
-    console.log('testing debug etc');
-    const user: User = await User.getUser(userId);
-    if (user == null) {
-      Logger.warn('Received invalid user id [' + userId + '] from ' + source);
-      return undefined;
+    private logger: Logger = new Logger('Util');
+
+    //EZ MESSAGE:
+
+    public notify(userIds: number[], channel: string, args: object) {
+        const obj: microServiceDTO = {
+            eventPattern: channel,
+            userIDs: userIds,
+            data: args,
+        };
+        this.gateway.emit('chat', obj);
     }
-    return user;
-  }
 
-  /**
-   * Checks if a user is in a channel, if this returns true it will display an error in the warning log
-   * @param channel channel to check in
-   * @param userId user to check for
-   * @param source calling event
-   * @param invert if this check should be inverted
-   * @private
-   */
-  public userInChannel(
-    channel: Channel,
-    userId: number,
-    source: string,
-    invert?: boolean,
-  ): boolean {
-    if (invert) {
-      if (!channel.hasUser(userId)) {
-        Logger.warn('User [' + userId + '] should be in channel for ' + source);
+    //EZ ERRORS:
+
+    public getChannel(channelId: number, source: string): Channel {
+        const channel: Channel = Channel.getChannel(channelId);
+        if (channel == null) {
+            this.logger.warn(`Received invalid channel id [${channelId}] from [${source}]`);
+            return undefined;
+        }
+        return channel;
+    }
+
+    public async getUser(userId: number, source: string): Promise<User | undefined> {
+        const user: User = await User.getUser(userId);
+        if (user == null) {
+            this.logger.warn(`Received invalid user id [${userId}] from [${source}]`);
+            return undefined;
+        }
+        return user;
+    }
+
+    /**
+     * Checks if a user is in a channel, if this returns true it will display an error in the warning log
+     * @param channel channel to check in
+     * @param userId user to check for
+     * @param source calling event
+     * @param invert if this check should be inverted
+     * @private
+     */
+    public userInChannel(channel: Channel, userId: number, source: string, invert?: boolean): boolean {
+        if (invert) {
+            if (!channel.hasUser(userId)) {
+                this.logger.debug(`User [${userId}] should be in channel for ${source}`);
+                return false;
+            }
+            return true;
+        }
+
+        if (channel.hasUser(userId)) {
+            this.logger.debug(`User [${userId}] should not be in channel for ${source}`);
+            return true;
+        }
         return false;
-      }
-      return true;
     }
 
-    if (channel.hasUser(userId)) {
-      Logger.warn(
-        'User [' + userId + '] should not be in channel for ' + source,
-      );
-      return true;
+    /**
+     * Checks if a user is not an admin in a channel
+     * @param channel
+     * @param actorId
+     * @param source
+     * returns true if user is *not* an admin
+     */
+    public notAdmin(channel: Channel, actorId: number, source: string): boolean {
+        if (!channel.isAdmin(actorId)) {
+            this.logger.debug(`Actor [${actorId}] is not an admin and can't issue the request ${source}`);
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * Checks if a user is not an admin in a channel
-   * @param channel
-   * @param actorId
-   * @param source
-   * returns true if user is *not* an admin
-   */
-  public notAdmin(channel: Channel, actorId: number, source: string): boolean {
-    if (!channel.isAdmin(actorId)) {
-      Logger.warn(
-        'Actor [' +
-          actorId +
-          "] is not an admin and can't issue the request " +
-          source,
-      );
-      return true;
+    /**
+     * Checks if a user does not own the specified channel
+     * @param channel
+     * @param actorId
+     * @param source
+     * returns true if user is *not* an owner
+     */
+    public notOwner(channel: Channel, actorId: number, source: string): boolean {
+        if (!channel.isOwner(actorId)) {
+            this.logger.debug(`Actor [${actorId}] is not an owner and can't issue the request ${source}`);
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
-
-  /**
-   * Checks if a user does not own the specified channel
-   * @param channel
-   * @param actorId
-   * @param source
-   * returns true if user is *not* an owner
-   */
-  public notOwner(channel: Channel, actorId: number, source: string): boolean {
-    if (!channel.isOwner(actorId)) {
-      Logger.warn(
-        'Actor [' +
-          actorId +
-          "] is not an owner and can't issue the request " +
-          source,
-      );
-      return true;
-    }
-    return false;
-  }
 }
