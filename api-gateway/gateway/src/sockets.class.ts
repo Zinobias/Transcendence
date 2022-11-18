32bf99421dc272ba5cee83adec240b0b8155d020
zinobias <zinobias@live.nl>
2022-11-18 16:04:10 +0100

    Added typing to map

--- api-gateway/gateway/src/sockets.class.ts
+++ api-gateway/gateway/src/sockets.class.ts
@@ -3,17 +3,17 @@ import { Socket } from 'socket.io';
 
 @Injectable()
 export class Sockets {
-  private static socketMap = new Map();
+  private static socketMap : Map<number, Socket>= new Map<number, Socket>();
 
   public storeSocket(userId: number, socket: Socket) {
     Sockets.socketMap.set(userId, socket);
   }
 
-  public getSocket(userId: number): Socket | null {
+  public getSocket(userId: number): Socket | undefined {
     if (Sockets.socketMap.has(userId)) {
       return Sockets.socketMap.get(userId);
     }
-    return null;
+    return undefined;
   }
 
   public sendData(users: number[], pattern: string, payload: object) {
