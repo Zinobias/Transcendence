67c191e3003d1007d60aad6cf62cb8dbb43db4fc
Abdoulaye Ba <abba@f0r3s24.codam.nl>
2022-11-28 19:14:59 +0100

    added avatar table in user-table

--- api-gateway/gateway/src/database/entities/user-table.ts
+++ api-gateway/gateway/src/database/entities/user-table.ts
@@ -1,27 +1,28 @@
-import {Column, Entity, OneToMany, PrimaryColumn} from 'typeorm';
-import {Sessions} from './sessions';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Sessions } from './sessions';
 
 @Entity('users')
 export class UserTable {
-    @PrimaryColumn()
-    userId: number;
-    @Column()
-    userName: string;
-    @Column({default: new Date().getTime(), type: 'bigint'})
-    createAt: number;
-    @OneToMany(() => Sessions, (session) => session.user, {
-        onDelete: 'CASCADE',
-    })
-    session: Sessions[];
+  @PrimaryColumn()
+  userId: number;
+  @Column()
+  userName: string;
+  @Column({ default: new Date().getTime(), type: 'bigint' })
+  createAt: number;
+  @OneToMany(() => Sessions, (session) => session.user, {
+    onDelete: 'CASCADE',
+  })
+  session: Sessions[];
 
-    constructor(userId: number, userName: string) {
-        this.userId = userId;
-        this.userName = userName;
-    }
+  constructor(userId: number, userName: string) {
+    this.userId = userId;
+    this.userName = userName;
+  }
+  @Column({ nullable: true, type: 'bytea' })
+  avatar: Uint8Array;
+  // @OneToMany(() => Sessions, (sessions: Sessions) => sessions.userId)
+  // public sessions: Sessions[];
 
-    // @OneToMany(() => Sessions, (sessions: Sessions) => sessions.userId)
-    // public sessions: Sessions[];
-
-    // @Column({ type: 'longblob' })
-    // avatar: Buffer;
+  // @Column({ type: 'longblob' })
+  // avatar: Buffer;
 }
