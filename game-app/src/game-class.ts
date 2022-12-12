1fd87a213c56baebe51710ae25478aeaf26b0577
zinobias <zinobias@live.nl>
2022-12-12 19:24:59 +0100

    Removing listener differently

--- game-app/src/game-class.ts
+++ game-app/src/game-class.ts
@@ -60,7 +60,7 @@ export class Game {
 		//);
 		logger.log(`GAME INSTANCE GAME ID : ${this.gameId}`);
 		this.eventEmitter.emit('game.ended', {gameId : this.gameId, payload: this.results });
-		this.eventEmitter.off("game.player.move." + this.gameId, this.setPlayerMovementState); 
+		this.eventEmitter.off("game.player.move." + this.gameId, this.setPlayerMovementState.bind(this)); 
 		return ;
 	}
 	// DTO for this should be
